9235e8e16a71b1d1101fc9ef009f23981f8103c7
Pinaki Poddar <Pinaki.Poddar@genesys.com>
2020-11-23 09:44:24 +0530

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
 #/bin/sh
+# -----------------------------------------------------
+# Deploys the application
+# The Docker image will be built from Github
+# All local files will be commited
+# -----------------------------------------------------
 PEM=learning_key.pem 
 USER=azureuser
 IP=20.190.44.106
 IMAGE=learn
-GIT_URL=http://github.com/ppoddar/learn.git
+GIT_URL=https://github.com/ppoddar/learn.git#main
+
 ssh -i $PEM $USER@$IP << EOFSSH
   docker build -t $IMAGE $GIT_URL
-  docker run -p80:80 $IMAGE
+  docker run -p 80:80 $IMAGE
 EOFSSH
